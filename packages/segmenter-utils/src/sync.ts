ac0cdd4112ed517a37b0f7dadb02c63eeac81c81
hikiko4ern <25303622+hikiko4ern@users.noreply.github.com>
2024-09-02 22:40:55 +0700

    chore(deps): update `nodejs` and `pnpm`

--- packages/segmenter-utils/src/sync.ts
+++ packages/segmenter-utils/src/sync.ts
@@ -1,6 +1,6 @@
 import { getFirstWord, initSync, segmentWords } from '../lib/segmenter_utils_rs';
 import wasm from '../lib/segmenter_utils_rs_bg.wasm?arraybuffer';
 
-initSync(wasm);
+initSync({ module: wasm });
 
 export { getFirstWord, segmentWords };
